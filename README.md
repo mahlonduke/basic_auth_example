84c224d7c51ee83b5cb2f2e26fa5fe9f28a54d22
mahlonduke <53539373+mahlonduke@users.noreply.github.com>
2020-03-11 14:35:42 -0700

    Created the example file, and anonymized the api_config.py sample

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # basic_auth_example
+This is an example of how to use basic authentication with the Accelo API.
+
+Basic authentication is great for automated apps and services where you can't use OAuth.
+
+
+--Directions--
+- Register an application in your Accelo account (Configuration -> API -> Register Application)
+- Copy your new application's Client ID and Client Secret, and insert them into api_config.py
+- Edit basic_auth_example.py and change "deployment" to your deployment name.  Do not include ".accelo.com"
+- Run basic_auth_example.py
--- basic_auth_example.py
+++ basic_auth_example.py
@@ -1,58 +1,58 @@
 # Import library dependencies
-import pandas as pd
-import numpy as np
 import requests
 import json
-import pprint as pp
-import math
-import re
 import base64
 
-# API Variables
-deployment = 'internal-zapier-testing'
-clientId = 'da14af208e@internal-zapier-testing.accelo.com'
-clientSecret = 'Sb7ejmW0UDF3hwUw6UiW0QuBuD7vqAxS'
+# Local file dependencies
+import api_config
 
-# Query Defaults
-email = 'customer.owner.affinitylive@gmail.com'
-companyId= '2195'
+# API Variables
+deployment = 'your-deployment'
+clientId = api_config.clientId
+clientSecret = api_config.clientSecret
 
 # API Endpoint Setup
-urlAffiliations = f'https://{deployment}.api.accelo.com/api/v0/affiliations?_fields=id,email,company&_search={email}&_limit=1'
-urlCompanies = f'https://{deployment}.api.accelo.com/api/v0/companies?_filters=id({companyId})&_fields=name&_limit=1'
+urlAffiliations = f'https://{deployment}.api.accelo.com/api/v0/companies?_fields=id,name&_limit=1'
+urlAuthorize = f'https://{deployment}.api.accelo.com/oauth2/v0/token'
 
+# Encode the Client ID and Secret
 preCoding = f'{clientId}:{clientSecret}'
 encoded = str(base64.b64encode(preCoding.encode("utf-8")), "utf-8")
 
-
-urlAuthorize = f'https://{deployment}.api.accelo.com/oauth2/v0/token'
-
-# API Header Setup
+# API Request Setup - for authentication
 authHeaders = {'Authorization': f'Basic {encoded}',
           'Content-Type': 'application/json'}
-
-# API Body Setup
 body = {
     "grant_type": "client_credentials",
     "scope": "read(all)",
     "expires_in": "3600"
 }
 
+# Print the details for debugging
 print("--------------------Debugging Details------------------------------")
+# App details to be used for authentication
 print("Authentication:")
 print(f'Client ID: {clientId}')
 print(f'Client Secret: {clientSecret}')
-print(f'Encoded ID:Secret: {encoded}\n\n')
+print(f'Encoded ID & Secret: {encoded}\n\n')
 
+# Authentication request details
 print(f'Token Request Header: {authHeaders}')
 print(f'Token Request Body: {body}\n\n')
+
+# Request a new token from the Accelo API, using the app authentication details
 response = requests.post(urlAuthorize, headers=authHeaders, data=json.dumps(body)).json()
+
+# Store the token that the Accelo API returns
 token = response['access_token']
 print(f'Access Token returned when supplying encoded ID & secret: {token}')
+
 # API Header Setup Using new Token
 headers = {'Content-Type': 'application/json',
           'Authorization': f'Bearer {token}'}
 print(f'Request Header Using new Token: {headers}')
+
+# Make a test request of the API, and print the result
 response = requests.get(urlAffiliations, headers=headers).json()
-print(f'Response when making a request using the new token: {response}')
+print(f'Response from the API when making a request using the new token:   {response["meta"]["message"]}')
 print("------------------End Debugging Details------------------------------")
