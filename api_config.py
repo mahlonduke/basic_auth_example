671b4b97212f07ed150c69dbd2dd1a2d09d48e9a
mahlonduke <53539373+mahlonduke@users.noreply.github.com>
2020-03-11 11:50:33 -0700

    Created initial files

---
+++ api_config.py
@@ -0,0 +1,3 @@
+# Accelo API Config, pulled from the Registered Applications page in the target deployment
+clientId = 'da14af208e@internal-zapier-testing.accelo.com'
+clientSecret = 'Sb7ejmW0UDF3hwUw6UiW0QuBuD7vqAxS'
